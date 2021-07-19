be7b95dc01f541e266fefef4fc8218aa77d6a608
dengbo <boris.deng@elitesland.com>
2021-07-19 23:35:31 +0800

    feat: login serve

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateAccountInput } from './dtos/create-account.dto';
+import { LoginInput } from './dtos/login.dto';
 import { User } from './entities/user.entity';
 
 Injectable();
@@ -14,7 +15,7 @@ export class UsersService {
     email,
     password,
     role,
-  }: CreateAccountInput): Promise<[boolean, string?]> {
+  }: CreateAccountInput): Promise<{ ok: boolean; error?: string }> {
     /**
      * check that email does not exist
      * create user & hash the password
@@ -23,13 +24,41 @@ export class UsersService {
       const exists = await this.users.findOne({ email });
       if (exists) {
         // make error
-        return [false, 'There is user with that email already'];
+        return { ok: false, error: 'There is user with that email already' };
       }
       await this.users.save(this.users.create({ email, password, role }));
-      return [true];
+      return { ok: true };
     } catch (e) {
       // make error
-      return [false, "Couldn't create account"];
+      return { ok: false, error: "Couldn't create account" };
+    }
+  }
+
+  async login({
+    email,
+    password,
+  }: LoginInput): Promise<{ ok: boolean; error?: string; token?: string }> {
+    // find the user with the email
+    // check if the password is correct
+    // make a JWT and give it to the user
+    try {
+      const user = await this.users.findOne({ email });
+      if (!user) {
+        return {
+          ok: false,
+          error: 'User not found',
+        };
+      }
+      const passwrodCorrect = await user.checkPassword(password);
+      if (!passwrodCorrect) {
+        return {
+          ok: false,
+          error: 'Wrong password',
+        };
+      }
+      return { ok: true, token: 'lalala' };
+    } catch (error) {
+      return { ok: false, error };
     }
   }
 }
