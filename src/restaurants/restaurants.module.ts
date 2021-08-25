3a7d85a7f870a2da4dac3e540a8f5a096668ad24
dengbo <boris.deng@elitesland.com>
2021-08-25 22:25:37 +0800

    feat: add category query service

--- src/restaurants/restaurants.module.ts
+++ src/restaurants/restaurants.module.ts
@@ -2,11 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Restaurant } from './entities/restaurant.entity';
 import { CategoryRepository } from './repositories/category.repository';
-import { RestaurantResolver } from './restaurants.resolver';
+import { CategoryResolver, RestaurantResolver } from './restaurants.resolver';
 import { RestaurantService } from './restautants.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Restaurant, CategoryRepository])],
-  providers: [RestaurantService, RestaurantResolver],
+  providers: [RestaurantService, RestaurantResolver, CategoryResolver],
 })
 export class RestaurantsModule {}
